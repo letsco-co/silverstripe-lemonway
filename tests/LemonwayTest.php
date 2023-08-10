24569d21c9c70d8f734c93aedbda38db6c92c528
Johann <johann.delebarre@letsco.co>
2023-08-10 14:54:04 +0200

    Create module

---
+++ tests/LemonwayTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Letsco\Lemonway\Tests;
+
+use SilverStripe\Dev\SapphireTest;
+
+class LemonwayTest extends SapphireTest
+{
+    public function test()
+    {
+        $this->assertEquals(1, intval("1"));
+    }
+}
